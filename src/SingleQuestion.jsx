a316163174b0c8b680d5c022abfc60f78a5e824e
Germain Burchfield <germainburchfield@Germains-iMac.attlocal.net>
2019-12-15 10:03:46 -0500

    Add previous button

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -10,7 +10,7 @@ export class SingleQuestion extends React.Component {
           isComplete: false
         };
         this.handleNextButtonClick = this.handleNextButtonClick.bind(this);
-
+        this.handlePreviousButtonClick = this.handlePreviousButtonClick.bind(this);
     }
 
     //This is where we left off Need to 
@@ -29,6 +29,12 @@ export class SingleQuestion extends React.Component {
         let nextQuestionNumber =  this.getQuestionNumber(currentQuestion) + 1;
         let nextQuestionName = 'q'+ nextQuestionNumber;
         return nextQuestionName;
+    }
+
+    getPreviousQuestionName = (currentQuestion) =>{
+        let previousQuestionNumber =  this.getQuestionNumber(currentQuestion) -1;
+        let previousQuestionName = 'q'+ previousQuestionNumber;
+        return previousQuestionName;
     }
 
     handleNextButtonClick  = (e) =>{
@@ -58,24 +64,68 @@ export class SingleQuestion extends React.Component {
         
     }
 
+    handlePreviousButtonClick = (e) =>{
+        let previousQuestionName = this.getPreviousQuestionName(this.props.questionName);
+        let currentQuestionNumber = this.getQuestionNumber(this.props.currentQuestion);
+        let  totalQuestions = this.getTotalQuestions();
+        let previousQuestionNumber = this.getQuestionNumber(previousQuestionName);
+
+        if(previousQuestionNumber == totalQuestions)
+        {
+             this.setState({isComplete:true});
+             this.setState({isVisible:false});
+             //this.setState({isVisible: false});
+             console.log("HHHHHHHH")
+        }
+
+        if(this.props.currentQuestion == this.props.questionName)
+        {   
+            this.props.showNextQuestion(previousQuestionName);
+            this.setState({isVisible: false});
+            
+        }
+
+    }
+
+    handleFinalPagePreviousClick = () =>{
+        this.props.showNextQuestion(this.props.questionName);
+        this.setState({isVisible: false});
+        this.setState({isComplete:false});
+    } 
+
     render(){
+
+        let firstQuestion = 
+        <div key={this.props.key +'i'} className="card" name={this.props.questionName}> 
+            <p>Tasks {this.getQuestionNumber(this.props.questionName)+1}/{this.getTotalQuestions()}</p>
+            {this.props.question}
+            {this.props.answers.map((answer)=> <Answer userAnswer={answer}/>)}
+            <span>
+                <button onClick={this.handleNextButtonClick}>Next Task</button>
+            </span>
+        </div>;
     
         let singleQuestion =
-         <div key={this.props.key +'i'} className="card" name={this.props.questionName}> 
-        <p>Tasks {this.getQuestionNumber(this.props.questionName)+1}/{this.getTotalQuestions()}</p>
-        {this.props.question}
-        {this.props.answers.map((answer)=> <Answer userAnswer={answer}/>)}
-        <span>
-            <button onClick={this.handleNextButtonClick}>Next Task</button>
-            <button>Previous Task</button>
-        </span>
-         </div>;
+        <div key={this.props.key +'i'} className="card" name={this.props.questionName}> 
+            <p>Tasks {this.getQuestionNumber(this.props.questionName)+1}/{this.getTotalQuestions()}</p>
+            {this.props.question}
+            {this.props.answers.map((answer)=> <Answer userAnswer={answer}/>)}
+            <span>
+                <button onClick={this.handleNextButtonClick}>Next Task</button>
+                <button onClick={this.handlePreviousButtonClick}>Previous Task</button>
+            </span>
+        </div>;
+
+         let displayQuestion = this.props.questionName == 'q0'?firstQuestion:singleQuestion;
 
         let finito =
-                <div key={'end'}className="card" name={'end'}> 
+             <div key={'end'}className="card" name={'end'}> 
                 <p>Thank you for completing The Guild Technology Module</p>
-                </div>;
+                <span>
+                    <button onClick={this.handleFinalPagePreviousClick}>Previous Task</button>
+                </span>
+            </div>;
 
-          return(this.state.isComplete?finito:singleQuestion)
+          return(this.state.isComplete?finito:displayQuestion)
     }
 }
